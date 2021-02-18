00f0d8b2a725ef24827c63df85c12e6751bd272b
Mohit <mohitrakhade20@gmail.com>
2021-02-18 23:03:18 +0530

    Updates

---
+++ precache-manifest.1265f45a03b7aaf18c145eaefe98186a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0431d5546bb0cb851c72f18b8c8a3047",
+    "url": "/xD/index.html"
+  },
+  {
+    "revision": "afea335cb574f9344bd6",
+    "url": "/xD/static/css/main.412f7ad3.chunk.css"
+  },
+  {
+    "revision": "9b36973b858da273f827",
+    "url": "/xD/static/js/2.cd9207be.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/xD/static/js/2.cd9207be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afea335cb574f9344bd6",
+    "url": "/xD/static/js/main.2e97fab2.chunk.js"
+  },
+  {
+    "revision": "8017019fc91f560d31dd",
+    "url": "/xD/static/js/runtime-main.7243ec93.js"
+  },
+  {
+    "revision": "ce77e5540b0923d66235084ec015f3ee",
+    "url": "/xD/static/media/1p.ce77e554.png"
+  },
+  {
+    "revision": "968882f833fbfdf3264ad67fcff3da9f",
+    "url": "/xD/static/media/2p.968882f8.png"
+  },
+  {
+    "revision": "c777029effabfe802f4dcbe90165b19e",
+    "url": "/xD/static/media/3p.c777029e.png"
+  },
+  {
+    "revision": "7a3ba182c36827b13b63d6fe6a052509",
+    "url": "/xD/static/media/4p.7a3ba182.png"
+  },
+  {
+    "revision": "c20addc7e232a02ddab52f7c4f2d2549",
+    "url": "/xD/static/media/5p.c20addc7.png"
+  },
+  {
+    "revision": "320fa7ccea11a1c8237792885da3feb7",
+    "url": "/xD/static/media/6p.320fa7cc.png"
+  },
+  {
+    "revision": "482bbd924590b2bb2e3ee3224c771fc3",
+    "url": "/xD/static/media/7p.482bbd92.png"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/xD/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/xD/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "8586c86185ae0664f9f52f87b22d688b",
+    "url": "/xD/static/media/curology-min.8586c861.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/xD/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "950211d91031fe423a7b2386766aa5d5",
+    "url": "/xD/static/media/p.950211d9.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/xD/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "d92246d226ad04a268df4d89fa76a7ce",
+    "url": "/xD/static/media/yourspace-min.d92246d2.png"
+  }
+]);
